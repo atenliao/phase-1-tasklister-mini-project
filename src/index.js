97528b24d2e07051970da479efb277191345a422
Zhongming <atenliao@gmail.com>
2023-04-06 18:19:24 -0700

    complete index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let form = document.querySelector('form')
+  form.addEventListener('submit',(e)=>{
+    e.preventDefault()
+    handleInput(e.target['new-task-description'].value)
+    form.reset()
+  })
 });
+
+
+function handleInput(todo){
+  console.log(todo)
+  let li = document.createElement('li');
+  let btn = document.createElement('button');
+  btn.addEventListener('click',HandleDelete);
+  btn.textContent='x';
+  li.textContent = `${todo} `;
+  li.appendChild(btn)
+  document.querySelector('#tasks').appendChild(li)
+}
+
+function HandleDelete(e){
+  e.target.parentNode.remove()
+}
